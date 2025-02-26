2bf85157ef051fdc8403c056b380d1b192535344
Bobby Triputa Majid <bobytriputramajid@gmail.com>
2025-02-26 12:11:26 +0700

    Menambahkan file awal proyek
    - Membuat struktur direktori dan template header (_partials/_template/header.php)
    - Menambahkan halaman utama (home/home.php)
    - Menyertakan aset gambar dan video (image/AI.png, image/logo_s (1).png, image/videoai.mp4)
    - Menyiapkan file index.php sebagai titik masuk utama

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$page=isset($_GET['page']) ?$_GET['page'] :'home';
+
+function loadPage($page): void{
+  switch($page){
+    case 'home':
+      include 'file_/home.php';
+      case 'login':
+        include 'file_/login.php';
+        default :
+        echo"404 -halaman tidak tersedia";
+        break;
+
+
+  }
+}
